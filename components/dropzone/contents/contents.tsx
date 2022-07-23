c70207f115e67994e8ca1888722e5d7778133b86
taikou-kobayashi-mg <taiko.kobayashi@m-gild.com>
2022-07-24 01:27:13 +0900

    wip:dropzone-upload

--- components/dropzone/contents/contents.tsx
+++ components/dropzone/contents/contents.tsx
@@ -1,18 +1,45 @@
 import { Img } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-// import { useProfileImageNew, useProfileImageDelete } from 'usecase/profile';
-// import { ProfileEditFormInput } from './types/form';
+import { useImageNew } from 'usecase/useImageNew';
 import styles from './contents.module.scss';
 
 export const Contents = () => {
-  const onDrop = (acceptedFiles: File[]) => {
-    const blobUrls = acceptedFiles.map((file) => URL.createObjectURL(file));
-    setPreviewImagePaths(blobUrls);
-  };
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
   // ブラウザ表示用の paths
   const [previewImagePaths, setPreviewImagePaths] = useState<string[]>();
+  // upload用の files
+  const [files, setFiles] = useState<File[]>();
+
+  const { imageNewHandler } = useImageNew();
+
+  //*********************
+  /** 選択された画像を処理 */
+  //*********************
+  const onDrop = (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+    const dataUrls = acceptedFiles.map((file) => URL.createObjectURL(file));
+    setPreviewImagePaths(dataUrls);
+  };
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  const handleClickDelete = () => {
+    setPreviewImagePaths([]);
+  };
+
+  const handleClickUpload = async () => {
+    const formData = buildFormData(files);
+    const response = await imageNewHandler(formData);
+  };
+
+  const buildFormData = (files?: File[]) => {
+    if (!files) {
+      return new FormData();
+    }
+    // DB へアップロードするために、FormData へ append する
+    const formData = new FormData();
+    files.forEach((file) => formData.append(file.name, file));
+    return formData;
+  };
 
   return (
     <>
@@ -30,6 +57,12 @@ export const Contents = () => {
             <Img src={image} />
           </div>
         ))}
+      <div>
+        <button onClick={handleClickDelete}>Delete preview images</button>
+      </div>
+      <div>
+        <button onClick={handleClickUpload}>Upload images</button>
+      </div>
     </>
   );
 };
