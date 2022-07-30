5a7a4f81ac0ae170a2fb10b5fd873fe7d869ae02
tak001 <twitwitwi2@yahoo.co.jp>
2022-07-30 10:07:31 +0900

    feat:modal pattern

--- components/modalPattern/contents/contents.tsx
+++ components/modalPattern/contents/contents.tsx
@@ -1,10 +1,12 @@
-import { Img } from '@chakra-ui/react';
+import { Img, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { useImageNew } from 'usecase/useImageNew';
+import { ImageModal } from '../modal/modal';
 import styles from './contents.module.scss';
 
 export const Contents = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   // ブラウザ表示用の paths
   const [previewImagePaths, setPreviewImagePaths] = useState<string[]>();
   // upload用の files
@@ -15,12 +17,11 @@ export const Contents = () => {
   //*********************
   /** 選択された画像を処理 */
   //*********************
-  const onDrop = (acceptedFiles: File[]) => {
+  const selectImages = (acceptedFiles: File[]) => {
     setFiles(acceptedFiles);
     const dataUrls = acceptedFiles.map((file) => URL.createObjectURL(file));
     setPreviewImagePaths(dataUrls);
   };
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   const handleClickDelete = () => {
     setPreviewImagePaths([]);
@@ -41,28 +42,32 @@ export const Contents = () => {
     return formData;
   };
 
+  const handleClickModal = () => {
+    onOpen();
+  };
+
   return (
-    <>
-      <div className={styles.contents}>
-        <div {...getRootProps({ className: 'dropzone' })}>
-          <input {...getInputProps()} accept="image/*" />
-          <p>
-            Drag and drop some image files here, or click to select image files
-          </p>
-        </div>
-      </div>
+    <div className={styles.contents}>
       {previewImagePaths &&
         previewImagePaths.map((image, i) => (
           <div key={i}>
             <Img src={image} />
           </div>
         ))}
+      <ImageModal
+        isOpen={isOpen}
+        onClose={onClose}
+        selectImages={selectImages}
+      />
       <div className={styles.contents__button}>
         <button onClick={handleClickDelete}>Delete preview images</button>
       </div>
       <div className={styles.contents__button}>
         <button onClick={handleClickUpload}>Upload images</button>
       </div>
-    </>
+      <div className={styles.contents__button}>
+        <button onClick={handleClickModal}>Open modal</button>
+      </div>
+    </div>
   );
 };
